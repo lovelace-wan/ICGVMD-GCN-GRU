f4f11377891dca60e791ce5da8c65116aca54d24
lovelace-wan <115611030+lovelace-wan@users.noreply.github.com>
2025-07-26 21:57:54 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,65 @@
+import torch
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GraphConvolution(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        try:
+            input = input.float()
+        except:
+            pass
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+
+class GCN(nn.Module):
+    """
+    A Two-layer GCN.
+    """
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj, use_relu=True):
+        x = self.gc1(x, adj)
+        if use_relu:
+            x = F.relu(x)
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return x
+
+
+
+
+
+
+
+
+
+
+
